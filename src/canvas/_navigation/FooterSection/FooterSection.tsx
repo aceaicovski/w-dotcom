a0b9b67086ad5f16a07d8ae04cf3e27ff10bd3bf
Yevhen Kolesnikov <yevhen.code@gmail.com>
2023-12-27 13:52:12 +0200

    add footerPlaceholder

--- src/canvas/_navigation/FooterSection/FooterSection.tsx
+++ src/canvas/_navigation/FooterSection/FooterSection.tsx
@@ -4,8 +4,8 @@ import { FooterSectionProps } from '.';
 
 export const FooterSection: FC<FooterSectionProps> = ({ title }) => (
   <div>
-    <span className="footer-title">{title}</span>
-    <ul>
+    <span className="footer-title opacity-100 text-white normal-case text-lg lg:text-xl xl:text-2xl">{title}</span>
+    <ul className="list-none mt-4 space-y-4">
       <UniformSlot name="links" />
     </ul>
   </div>
