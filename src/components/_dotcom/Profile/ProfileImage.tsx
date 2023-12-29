ec3d08834bcc23616963932f074a8164557c0f80
Ana Ceaicovski <ceaicovski94@gmail.com>
2023-12-29 13:32:05 +0200

    replaced px values by tailwind native ones

--- src/components/_dotcom/Profile/ProfileImage.tsx
+++ src/components/_dotcom/Profile/ProfileImage.tsx
@@ -6,7 +6,7 @@ export const ProfileImage = React.forwardRef<HTMLElement, ProfileImageProps>(({ 
   return (
     <div className="flex shrink-0 w-full h-auto rounded-lg md:row-span-2 lg:row-span-3">
       <figure ref={ref} className="block w-full rounded-lg">
-        <img {...props} className="w-full h-full md:w-[160px] md:h-[160px] lg:w-[200px] lg:h-[200px] rounded-lg" />
+        <img {...props} className="w-full h-full md:w-40 md:h-40 lg:w-52 lg:h-52 rounded-lg" />
       </figure>
     </div>
   );
